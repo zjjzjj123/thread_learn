30aef0939de15cd54aab93c5f4a258aca923c8b2
zjjzjj123 <283486907@qq.com>
2019-10-09 19:16:53 +0800

    thread learn

---
+++ 10.锁定匹配线程数执行.py
@@ -0,0 +1,17 @@
+'''
+指定数量的线程再执行
+'''
+import threading
+import time
+
+bar = threading.Barrier(3) #此时是只有匹配够两个线程才能执行
+
+def serve():
+    print(threading.current_thread().name,'start')
+    bar.wait()
+    time.sleep(5)
+    print(threading.current_thread().name,'end')
+
+for i in range(5):
+    threading.Thread(target=serve).start()
+
